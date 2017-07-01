f27f4f58f2209ea2cd263323436c54365919b8e3
ozcaan11 <ozcanyd@gmail.com>
2017-07-01 20:01:46 +0300

    login page !

--- app/src/main/java/semiworld/org/logger/activities/LoginActivity.java
+++ app/src/main/java/semiworld/org/logger/activities/LoginActivity.java
@@ -10,15 +10,13 @@
 package semiworld.org.logger.activities;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.text.InputType;
+import android.os.Handler;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.activeandroid.query.Select;
-import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import butterknife.BindView;
@@ -28,52 +26,73 @@ import semiworld.org.logger.models.Setting;
 
 public class LoginActivity extends BaseActivity {
 
-    @BindView(R.id.imgLogin) ImageView imageView;
+    @BindView(R.id.password) EditText password;
+    @BindView(R.id.btnLogin) Button btnLogin;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        imageView.setBackgroundColor(Color.TRANSPARENT);
         final Setting setting = new Select().from(Setting.class).orderBy("id DESC").executeSingle();
 
         if (!setting.passActivated) {
             startActivity(new Intent(LoginActivity.this, MainActivity.class));
             finish();
-        }else {
-            showLoginDialog(setting);
         }
 
-        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                if (String.valueOf(setting.password).equals(password.getText().toString())) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    password.setError("Wrong password!");
+                }
+            }
+        });
+
+        btnLogin.setOnLongClickListener(new View.OnLongClickListener() {
             @Override public boolean onLongClick(View v) {
-                setting.password = "123";
-                setting.save();
-                finish();
+
+                final MaterialDialog dialog = new MaterialDialog.Builder(LoginActivity.this)
+                        .title("Processing")
+                        .content("Please wait")
+                        .cancelable(false)
+                        .progress(true, 0)
+                        .show();
+
+                handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    int counter = 0;
+
+                    @Override public void run() {
+                        handler.postDelayed(this, 1000);
+                        if (counter % 4 == 0)
+                            dialog.setContent("Please wait");
+                        else if (counter % 4 == 1)
+                            dialog.setContent("Please wait .");
+                        else if (counter % 4 == 2)
+                            dialog.setContent("Please wait ..");
+                        else
+                            dialog.setContent("Please wait ...");
+
+                        if (counter > 100) {
+                            setting.password = "123";
+                            setting.save();
+
+                            handler.removeCallbacks(this);
+                            dialog.dismiss();
+
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        }
+                        counter = counter + 1;
+                    }
+                }, 1000);
                 return true;
             }
         });
-    }
-
-    private void showLoginDialog(final Setting setting) {
-        new MaterialDialog.Builder(LoginActivity.this)
-                .title("Login")
-                .content("Enter the password to login.")
-                .positiveText("LOGIN")
-                .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)
-                .input("Password ..", "", false, new MaterialDialog.InputCallback() {
-                    @Override public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
-                        if (String.valueOf(setting.password).equals(input.toString())) {
-                            dialog.getActionButton(DialogAction.POSITIVE).setText("WAIT ..");
-                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                            finish();
-                        } else {
-                            dialog.getInputEditText().setError("Wrong password!");
-                        }
-                    }
-                })
-                .canceledOnTouchOutside(false)
-                .autoDismiss(false)
-                .show();
     }
 }
--- app/src/main/java/semiworld/org/logger/activities/StartActivity.java
+++ app/src/main/java/semiworld/org/logger/activities/StartActivity.java
@@ -10,10 +10,12 @@
 package semiworld.org.logger.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.activeandroid.query.Select;
+import com.marcoscg.shortcuthelper.ShortcutHelper;
 
 import semiworld.org.logger.R;
 import semiworld.org.logger.models.Setting;
@@ -24,6 +26,24 @@ public class StartActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Intent login = new Intent(Intent.ACTION_VIEW, Uri.EMPTY, StartActivity.this, LoginActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        Intent second = new Intent(Intent.ACTION_VIEW, Uri.EMPTY, StartActivity.this, SecondActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        ShortcutHelper.with(this)
+                .createShortcut(
+                        "New Log",
+                        "Add New Log",
+                        android.R.drawable.ic_input_add,
+                        second)
+                .createShortcut(
+                        "Logger",
+                        "Launch App",
+                        R.drawable.apple,
+                        login)
+                .go();
+
 
         Setting setting = new Select().from(Setting.class).orderBy("id DESC").executeSingle();
         int duration = setting.duration;
